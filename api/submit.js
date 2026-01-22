6e2f1fe01c8299e36e1cf07d1a4db498328200bc
Muhammad Riziq Zhapran <134038828+nicwaproject@users.noreply.github.com>
2026-01-22 15:12:32 +0800

    update

--- api/submit.js
+++ api/submit.js
@@ -111,14 +111,12 @@ export default async function handler(req, res) {
         <p><strong>Client:</strong> ${meta?.clientName || "-"}</p>
       `,
       attachments: [
-        {
-          filename: fileName,
-          content: pdfBuffer.toString("base64"),
-          encoding: "base64",
-          type: "application/pdf",
-          disposition: "attachment"
-        },
-      ],
+      {
+        filename: fileName,
+        content: pdfBuffer.toString("base64"),
+        contentType: "application/pdf"
+      }
+    ],
     });
 
     return res.status(200).json({
